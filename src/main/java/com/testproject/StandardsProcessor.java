6efb08544539ce986198a1d7f3c8612a508ccbc1
tmitim <igetemail@gmail.com>
2014-07-24 23:50:36 -0700

    hello world

---
+++ src/main/java/com/testproject/StandardsProcessor.java
@@ -0,0 +1,11 @@
+package com.testproject;
+
+public class StandardsProcessor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
